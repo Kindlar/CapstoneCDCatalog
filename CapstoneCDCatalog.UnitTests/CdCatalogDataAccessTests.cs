4f82c7bb4992dd903aab374bfd747f05c819a8d3
Zanders <zanders@SEAXXZANDERSON.airgroup.alaskaair.com>
2016-01-22 14:26:50 -0800

    Added Unit Testing Project

---
+++ CapstoneCDCatalog.UnitTests/CdCatalogDataAccessTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CapstoneCDCatalog.UnitTests
+{
+    [TestFixture]
+    public class CdCatalogDataAccessTests
+    {
+
+        [Test]
+        public void Given()
+        {
+            
+        }
+    }
+}
---
+++ CapstoneCDCatalog/CDCatalogDataAccess.cs
@@ -0,0 +1,7 @@
+﻿namespace CapstoneCDCatalog
+{
+    public class CDCatalogDataAccess
+    {
+         
+    }
+}
